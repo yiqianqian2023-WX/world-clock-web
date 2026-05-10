031b6f41a4a2e3b3a240b80fe83e89dad04d3698
yiqianqian2023-WX <yiqianqian2023@gmail.com>
2026-05-10 21:55:32 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Service Worker for offline caching
+const CACHE = 'world-clock-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['worldclock.html', 'manifest.json', 'icon.svg'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
